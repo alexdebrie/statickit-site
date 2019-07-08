1f273f097994352607c0d3006b37034fbf5e6ead
Derrick Reimer <derrickreimer@gmail.com>
2019-07-08 14:14:39 -0700

    Use next link component

--- components/header.js
+++ components/header.js
@@ -1,11 +1,14 @@
-import ReactSVG from 'react-svg'
+import ReactSVG from "react-svg";
+import Link from "next/link";
 
 export default () => (
   <header className="mx-auto container px-6 py-4">
     <div className="flex items-center h-10">
-      <a href="/" className="mr-8 block flex-shrink-0">
-        <ReactSVG src="/static/logo.svg" />
-      </a>
+      <Link href="/">
+        <a className="mr-8 block flex-shrink-0">
+          <ReactSVG src="/static/logo.svg" />
+        </a>
+      </Link>
     </div>
   </header>
 )
