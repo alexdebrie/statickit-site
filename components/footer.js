bb23d4fa559d4dcc135565a73de402a5810fcb09
Derrick Reimer <derrickreimer@gmail.com>
2019-07-08 15:56:05 -0700

    Add footer

---
+++ components/footer.js
@@ -0,0 +1,14 @@
+import ReactSVG from 'react-svg';
+import Link from 'next/link';
+import Head from 'next/head';
+
+export default (props) => (
+  <footer className="border-t">
+    <div className="mx-auto container px-6 py-10">
+      <div className="flex items-center justify-start opacity-25">
+        <div className="mr-3"><ReactSVG src="/static/level.svg" /></div>
+        <div className="font-semibold">Level Technologies</div>
+      </div>
+    </div>
+  </footer>
+)
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Link from 'next/link';
 import Head from 'next/head';
 import Header from '../components/header';
+import Footer from '../components/footer';
 import CodeBlock from '../components/code_block';
 import '../styles/main.css';
 
@@ -28,44 +29,48 @@ function MyForm() {
   `;
 
   return (
-    <main className="antialiased">
-      <Head>
-        <meta property="og:title" content="Simple form hosting for static sites" />
-        <meta property="og:locale" content="en_US" />
-        <meta name="description" content="Architected for developer happiness. Coming July 2019." />
-        <meta property="og:description" content="Architected for developer happiness. Coming July 2019." />
-        <link rel="canonical" href="https://www.statickit.com/" />
-        <meta property="og:url" content="https://www.statickit.com/" />
-        <meta property="og:site_name" content="StaticKit" />
-        <meta property="og:image" content="https://www.statickit.com/static/logo-300.png" />
-      </Head>
-      <Header pageTitle="Simple form hosting for static sites" />
-      <div className="mx-auto pt-16 container">
-        <div className="pb-16 sm:pb-32 flex flex-wrap">
-          <div className="px-6 pb-16 sm:pt-12 sm:pb-0 w-full sm:w-1/2 xl:w-2/5">
-            <div className="pb-2">
-              <span className="text-sm bg-indigo-600 rounded-full px-3 py-1 text-white font-semibold">Introducing</span>
-            </div>
-            <h1 className="pb-6 text-5xl font-semibold leading-tight tracking-snug">Simple form hosting <br className="hidden sm:block xl:hidden"/>for static sites</h1>
-
-            <p className="pb-6 text-lg text-gray-700">Architected for developer happiness. <br className="hidden sm:block"/>Launching in July 2019 🚀</p>
-
-            <form id="opt-in-form" className="max-w-sm">
-              <div className="pb-2 flex items-center">
-                <input type="email" name="email" className="input-field block w-full" placeholder="Your email address" required />
-                <input type="submit" name="submit" className="btn" value="Notify me" disabled />
+    <div className="antialiased">
+      <main>
+        <Head>
+          <meta property="og:title" content="Simple form hosting for static sites" />
+          <meta property="og:locale" content="en_US" />
+          <meta name="description" content="Architected for developer happiness. Coming July 2019." />
+          <meta property="og:description" content="Architected for developer happiness. Coming July 2019." />
+          <link rel="canonical" href="https://www.statickit.com/" />
+          <meta property="og:url" content="https://www.statickit.com/" />
+          <meta property="og:site_name" content="StaticKit" />
+          <meta property="og:image" content="https://www.statickit.com/static/logo-300.png" />
+        </Head>
+        <Header pageTitle="Simple form hosting for static sites" />
+        <div className="mx-auto pt-16 container">
+          <div className="pb-16 sm:pb-32 flex flex-wrap">
+            <div className="px-6 pb-16 sm:pt-12 sm:pb-0 w-full sm:w-1/2 xl:w-2/5">
+              <div className="pb-2">
+                <span className="text-sm bg-indigo-600 rounded-full px-3 py-1 text-white font-semibold">Introducing</span>
               </div>
-              <div data-sk-error="email" className="font-bold text-red-600 text-sm"></div>
-            </form>
-          </div>
+              <h1 className="pb-6 text-5xl font-semibold leading-tight tracking-snug">Simple form hosting <br className="hidden sm:block xl:hidden"/>for static sites</h1>
 
-          <div className="px-6 w-full sm:w-1/2 xl:w-3/5">
-            <div className="mb-2 p-4 w-full rounded-lg text-sm leading-relaxed overflow-auto bg-code">
-              <CodeBlock className="language-jsx">{sampleCode.trim()}</CodeBlock>
+              <p className="pb-6 text-lg text-gray-700">Architected for developer happiness. <br className="hidden sm:block"/>Launching in July 2019 🚀</p>
+
+              <form id="opt-in-form" className="max-w-sm">
+                <div className="pb-2 flex items-center">
+                  <input type="email" name="email" className="input-field block w-full" placeholder="Your email address" required />
+                  <input type="submit" name="submit" className="btn" value="Notify me" disabled />
+                </div>
+                <div data-sk-error="email" className="font-bold text-red-600 text-sm"></div>
+              </form>
+            </div>
+
+            <div className="px-6 w-full sm:w-1/2 xl:w-3/5">
+              <div className="mb-2 p-4 w-full rounded-lg text-sm leading-relaxed overflow-auto bg-code">
+                <CodeBlock className="language-jsx">{sampleCode.trim()}</CodeBlock>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </main>
+      </main>
+
+      <Footer />
+    </div>
   )
 };
