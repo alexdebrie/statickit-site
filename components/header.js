133ce752d17a862f15d97e452508531ebeb7f416
Derrick Reimer <derrickreimer@gmail.com>
2019-07-08 15:52:54 -0700

    Flesh out home page

--- components/header.js
+++ components/header.js
@@ -1,8 +1,13 @@
-import ReactSVG from "react-svg";
-import Link from "next/link";
+import ReactSVG from 'react-svg';
+import Link from 'next/link';
+import Head from 'next/head';
 
-export default () => (
+export default (props) => (
   <header className="mx-auto container px-6 py-4">
+    <Head>
+      <title>{props.pageTitle} · StaticKit</title>
+      <link rel="shortcut icon" href="/static/favicon.png"></link>
+    </Head>
     <div className="flex items-center h-10">
       <Link href="/">
         <a className="mr-8 block flex-shrink-0">
