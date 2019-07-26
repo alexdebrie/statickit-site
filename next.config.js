9caf4730046066bb3ac7cc643cc4268b6fbe766d
Derrick Reimer <derrickreimer@gmail.com>
2019-07-26 10:51:42 -0500

    Add serverless target

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 const withCSS = require('@zeit/next-css')
 module.exports = withCSS({
+  target: 'serverless',
   env: {
     STATICKIT_URL: process.env.STATICKIT_URL,
     FORM_ID: process.env.FORM_ID
