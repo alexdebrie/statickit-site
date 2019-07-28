aa5cab22b47ca2c1999b5725e697ef6a5fdff7cb
Derrick Reimer <derrickreimer@gmail.com>
2019-07-28 08:12:31 -0500

    Refactor pricing page

--- pages/pricing.js
+++ pages/pricing.js
@@ -10,6 +10,36 @@ function PricingPage() {
   const title = 'Pricing';
   const description = 'Simple, honest pricing. Free for personal use.';
 
+  const tiers = {
+    individual: {
+      slug: "individual",
+      name: "Individual",
+      price: "$0",
+      usage: "Personal Use",
+      domains: "∞",
+      endpoints: "∞",
+      submissions: "∞"
+    },
+    singleSite: {
+      slug: "single",
+      name: "Single Site",
+      price: "$20",
+      usage: "Business",
+      domains: "1",
+      endpoints: "∞",
+      submissions: "10,000 / mo"
+    },
+    multiSite: {
+      slug: "multi",
+      name: "Multi Site",
+      price: "$60",
+      usage: "Business",
+      domains: "∞",
+      endpoints: "∞",
+      submissions: "20,000 / mo"
+    }
+  }
+
   return (
     <div className="font-sans antialiased text-gray-900">
       <main>
@@ -33,46 +63,46 @@ function PricingPage() {
           </div>
 
           <div className="pb-16">
-            <div className="w-full overflow-x-scroll sm:overflow-visible">
+            <div className="w-full hidden sm:block">
               <table className="mb-16 w-full sm:max-w-2xl mx-auto">
                 <thead>
                   <tr className="text-xs uppercase text-indigo-600">
                     <th></th>
-                    <th className="whitespace-no-wrap">Individual</th>
-                    <th className="whitespace-no-wrap">Business</th>
-                    <th className="whitespace-no-wrap">Agency</th>
+                    <th className="whitespace-no-wrap">{tiers.individual.name}</th>
+                    <th className="whitespace-no-wrap">{tiers.singleSite.name}</th>
+                    <th className="whitespace-no-wrap">{tiers.multiSite.name}</th>
                   </tr>
-                  <tr className="text-xl md:text-4xl tracking-snug">
+                  <tr className="text-2xl md:text-4xl tracking-snug">
                     <th className="border-b"></th>
-                    <th className="pb-3 font-semibold w-1/4 whitespace-no-wrap border-b">$0<span className="text-sm md:text-base font-normal text-gray-700"> / mo</span></th>
-                    <th className="pb-3 font-semibold w-1/4 whitespace-no-wrap border-b">$20<span className="text-sm md:text-base font-normal text-gray-700"> / mo</span></th>
-                    <th className="pb-3 font-semibold w-1/4 whitespace-no-wrap border-b">$60<span className="text-sm md:text-base font-normal text-gray-700"> / mo</span></th>
+                    <th className="pb-3 font-semibold w-1/4 whitespace-no-wrap border-b">{tiers.individual.price}<span className="text-base font-normal text-gray-700"> / mo</span></th>
+                    <th className="pb-3 font-semibold w-1/4 whitespace-no-wrap border-b">{tiers.singleSite.price}<span className="text-base font-normal text-gray-700"> / mo</span></th>
+                    <th className="pb-3 font-semibold w-1/4 whitespace-no-wrap border-b">{tiers.multiSite.price}<span className="text-base font-normal text-gray-700"> / mo</span></th>
                   </tr>
                 </thead>
                 <tbody>
                   <tr className="text-sm">
                     <td className="px-2 py-1 pt-4 font-bold">Usage</td>
-                    <td className="px-2 py-1 pt-4 text-center text-gray-700">Personal Use</td>
-                    <td className="px-2 py-1 pt-4 text-center text-gray-700">Business</td>
-                    <td className="px-2 py-1 pt-4 text-center text-gray-700">Business</td>
+                    <td className="px-2 py-1 pt-4 text-center text-gray-700">{tiers.individual.usage}</td>
+                    <td className="px-2 py-1 pt-4 text-center text-gray-700">{tiers.singleSite.usage}</td>
+                    <td className="px-2 py-1 pt-4 text-center text-gray-700">{tiers.multiSite.usage}</td>
                   </tr>
                   <tr className="text-sm border-b">
                     <td className="px-2 py-1 pb-4 font-bold">Domains</td>
-                    <td className="px-2 py-1 pb-4 text-center text-gray-700">∞</td>
-                    <td className="px-2 py-1 pb-4 text-center text-gray-700">1</td>
-                    <td className="px-2 py-1 pb-4 text-center text-gray-700">10</td>
+                    <td className="px-2 py-1 pb-4 text-center text-gray-700">{tiers.individual.domains}</td>
+                    <td className="px-2 py-1 pb-4 text-center text-gray-700">{tiers.singleSite.domains}</td>
+                    <td className="px-2 py-1 pb-4 text-center text-gray-700">{tiers.multiSite.domains}</td>
                   </tr>
                   <tr className="text-sm">
                     <td className="px-2 py-1 pt-4 font-bold">Endpoints</td>
-                    <td className="px-2 py-1 pt-4 text-center text-gray-700">∞</td>
-                    <td className="px-2 py-1 pt-4 text-center text-gray-700">∞</td>
-                    <td className="px-2 py-1 pt-4 text-center text-gray-700">∞</td>
+                    <td className="px-2 py-1 pt-4 text-center text-gray-700">{tiers.individual.endpoints}</td>
+                    <td className="px-2 py-1 pt-4 text-center text-gray-700">{tiers.singleSite.endpoints}</td>
+                    <td className="px-2 py-1 pt-4 text-center text-gray-700">{tiers.multiSite.endpoints}</td>
                   </tr>
                   <tr className="text-sm border-b">
                     <td className="px-2 py-1 pb-4 font-bold">Submissions</td>
-                    <td className="px-2 py-1 pb-4 text-center text-gray-700">∞</td>
-                    <td className="px-2 py-1 pb-4 text-center text-gray-700">10,000 / mo</td>
-                    <td className="px-2 py-1 pb-4 text-center text-gray-700">100,000 / mo</td>
+                    <td className="px-2 py-1 pb-4 text-center text-gray-700">{tiers.individual.submissions}</td>
+                    <td className="px-2 py-1 pb-4 text-center text-gray-700">{tiers.singleSite.submissions}</td>
+                    <td className="px-2 py-1 pb-4 text-center text-gray-700">{tiers.multiSite.submissions}</td>
                   </tr>
                   <tr>
                     <td></td>
@@ -80,10 +110,123 @@ function PricingPage() {
                       <a href="https://app.statickit.com/signup?plan=individual" className="btn btn-sm block w-full text-center">Join the Beta</a>
                     </td>
                     <td className="p-3">
-                      <a href="https://app.statickit.com/signup?plan=business" className="btn btn-sm block w-full text-center">Join the Beta</a>
+                      <a href="https://app.statickit.com/signup?plan=single" className="btn btn-sm block w-full text-center">Join the Beta</a>
                     </td>
                     <td className="p-3">
-                      <a href="https://app.statickit.com/signup?plan=business" className="btn btn-sm block w-full text-center">Join the Beta</a>
+                      <a href="https://app.statickit.com/signup?plan=multi" className="btn btn-sm block w-full text-center">Join the Beta</a>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+
+            <div className="w-full sm:hidden">
+              <table className="mb-16 w-full">
+                <thead>
+                  <tr className="text-xs uppercase text-indigo-600">
+                    <th></th>
+                    <th className="whitespace-no-wrap">{tiers.individual.name}</th>
+                  </tr>
+                  <tr className="text-2xl tracking-snug">
+                    <th className="border-b"></th>
+                    <th className="pb-3 font-semibold whitespace-no-wrap border-b">{tiers.individual.price}<span className="text-base font-normal text-gray-700"> / mo</span></th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr className="text-sm">
+                    <td className="px-2 py-1 pt-4 font-bold">Usage</td>
+                    <td className="px-2 py-1 pt-4 text-center text-gray-700">{tiers.individual.usage}</td>
+                  </tr>
+                  <tr className="text-sm border-b">
+                    <td className="px-2 py-1 pb-4 font-bold">Domains</td>
+                    <td className="px-2 py-1 pb-4 text-center text-gray-700">{tiers.individual.domains}</td>
+                  </tr>
+                  <tr className="text-sm">
+                    <td className="px-2 py-1 pt-4 font-bold">Endpoints</td>
+                    <td className="px-2 py-1 pt-4 text-center text-gray-700">{tiers.individual.endpoints}</td>
+                  </tr>
+                  <tr className="text-sm border-b">
+                    <td className="px-2 py-1 pb-4 font-bold">Submissions</td>
+                    <td className="px-2 py-1 pb-4 text-center text-gray-700">{tiers.individual.submissions}</td>
+                  </tr>
+                  <tr>
+                    <td></td>
+                    <td className="p-3">
+                      <a href="https://app.statickit.com/signup?plan=individual" className="btn btn-sm block w-full text-center">Join the Beta</a>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+
+              <table className="mb-16 w-full">
+                <thead>
+                  <tr className="text-xs uppercase text-indigo-600">
+                    <th></th>
+                    <th className="whitespace-no-wrap">{tiers.singleSite.name}</th>
+                  </tr>
+                  <tr className="text-2xl tracking-snug">
+                    <th className="border-b"></th>
+                    <th className="pb-3 font-semibold whitespace-no-wrap border-b">{tiers.singleSite.price}<span className="text-base font-normal text-gray-700"> / mo</span></th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr className="text-sm">
+                    <td className="px-2 py-1 pt-4 font-bold">Usage</td>
+                    <td className="px-2 py-1 pt-4 text-center text-gray-700">{tiers.singleSite.usage}</td>
+                  </tr>
+                  <tr className="text-sm border-b">
+                    <td className="px-2 py-1 pb-4 font-bold">Domains</td>
+                    <td className="px-2 py-1 pb-4 text-center text-gray-700">{tiers.singleSite.domains}</td>
+                  </tr>
+                  <tr className="text-sm">
+                    <td className="px-2 py-1 pt-4 font-bold">Endpoints</td>
+                    <td className="px-2 py-1 pt-4 text-center text-gray-700">{tiers.singleSite.endpoints}</td>
+                  </tr>
+                  <tr className="text-sm border-b">
+                    <td className="px-2 py-1 pb-4 font-bold">Submissions</td>
+                    <td className="px-2 py-1 pb-4 text-center text-gray-700">{tiers.singleSite.submissions}</td>
+                  </tr>
+                  <tr>
+                    <td></td>
+                    <td className="p-3">
+                      <a href="https://app.statickit.com/signup?plan=single" className="btn btn-sm block w-full text-center">Join the Beta</a>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+
+              <table className="mb-16 w-full">
+                <thead>
+                  <tr className="text-xs uppercase text-indigo-600">
+                    <th></th>
+                    <th className="whitespace-no-wrap">{tiers.multiSite.name}</th>
+                  </tr>
+                  <tr className="text-2xl tracking-snug">
+                    <th className="border-b"></th>
+                    <th className="pb-3 font-semibold whitespace-no-wrap border-b">{tiers.multiSite.price}<span className="text-base font-normal text-gray-700"> / mo</span></th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr className="text-sm">
+                    <td className="px-2 py-1 pt-4 font-bold">Usage</td>
+                    <td className="px-2 py-1 pt-4 text-center text-gray-700">{tiers.multiSite.usage}</td>
+                  </tr>
+                  <tr className="text-sm border-b">
+                    <td className="px-2 py-1 pb-4 font-bold">Domains</td>
+                    <td className="px-2 py-1 pb-4 text-center text-gray-700">{tiers.multiSite.domains}</td>
+                  </tr>
+                  <tr className="text-sm">
+                    <td className="px-2 py-1 pt-4 font-bold">Endpoints</td>
+                    <td className="px-2 py-1 pt-4 text-center text-gray-700">{tiers.multiSite.endpoints}</td>
+                  </tr>
+                  <tr className="text-sm border-b">
+                    <td className="px-2 py-1 pb-4 font-bold">Submissions</td>
+                    <td className="px-2 py-1 pb-4 text-center text-gray-700">{tiers.multiSite.submissions}</td>
+                  </tr>
+                  <tr>
+                    <td></td>
+                    <td className="p-3">
+                      <a href="https://app.statickit.com/signup?plan=multi" className="btn btn-sm block w-full text-center">Join the Beta</a>
                     </td>
                   </tr>
                 </tbody>
