384ed379cc011c3fcca03ab0a2f2181e7f1e374c
Derrick Reimer <derrickreimer@gmail.com>
2019-07-29 10:07:23 -0700

    Install MDX for markdown content

--- next.config.js
+++ next.config.js
@@ -1,8 +1,15 @@
-const withCSS = require('@zeit/next-css')
-module.exports = withCSS({
-  target: 'serverless',
-  env: {
-    STATICKIT_URL: process.env.STATICKIT_URL,
-    FORM_ID: process.env.FORM_ID
-  }
-})
+const withCSS = require('@zeit/next-css');
+const withMDX = require('@next/mdx')({
+  extension: /\.mdx?$/
+});
+
+module.exports = withMDX(
+  withCSS({
+    target: 'serverless',
+    env: {
+      STATICKIT_URL: process.env.STATICKIT_URL,
+      FORM_ID: process.env.FORM_ID
+    },
+    pageExtensions: ['js', 'jsx', 'mdx']
+  })
+);
