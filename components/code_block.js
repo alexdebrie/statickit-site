a523f66a0e6ea06c7fa1c576dd4907ca84b1a520
Derrick Reimer <derrickreimer@gmail.com>
2019-08-11 22:24:06 -0500

    Temporarily disable Prism

--- components/code_block.js
+++ components/code_block.js
@@ -1,16 +1,16 @@
 import React, { useState, useEffect } from 'react';
-import Prism from 'prismjs';
+// import Prism from 'prismjs';
 
 // This import is required to get JSX highlighting working properly:
 // https://github.com/PrismJS/prism/issues/1116#issuecomment-291921678
-import PrismJsx from 'prismjs/components/prism-jsx.min';
+// import PrismJsx from 'prismjs/components/prism-jsx.min';
 
 import '../styles/dracula.css';
 
 export default props => {
-  useEffect(() => {
-    Prism.highlightAll();
-  });
+  // useEffect(() => {
+  //   Prism.highlightAll();
+  // });
 
   return (
     <pre>
