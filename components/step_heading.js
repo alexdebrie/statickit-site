4d004fe63dd6c41023de47106807b70233984217
Derrick Reimer <derrickreimer@gmail.com>
2019-08-21 10:11:59 -0500

    Refine step heading

--- components/step_heading.js
+++ components/step_heading.js
@@ -1,6 +1,6 @@
 export default props => (
   <h2 className="flex items-center">
-    <div className="flex items-center justify-center mr-3 text-base bg-pink-600 w-8 h-8 rounded-full font-bold text-white">
+    <div className="flex items-center justify-center mr-3 text-base bg-green-200 w-8 h-8 rounded-full font-bold text-green-700">
       {props.step}
     </div>
     <div>{props.children}</div>
